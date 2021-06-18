e2d2d50fcf8be821f3fb973819d2c5b4b5d837db
Hikari Fujimoto <hikari.p.fujimoto@gmail.com>
2021-06-18 10:30:58 +0900

    Update modified greedy solution

--- Week 5/README.md
+++ Week 5/README.md
@@ -1,2 +1,17 @@
 # Google STEP 2021: Travelling Salesman Problem Challenges
 
+## Assignment
+Modify the greedy algorithm solving TSP to find a more optimized path.
+Original/sample program is given [here](https://github.com/hayatoito/google-step-tsp)
+
+## Algorithm
+In order to optimize the path, the modified greedy solution repeats the process of finding greedy solution starting from different starting point and two-opts the path before comparing with previous greedy solution to determine the shortest run.
+
+The overall process is as follows:
+- Randomly select a starting point within the given dataset
+- Determine the greedy solution (connecting the nearest unvisited points)
+- Run two-opt to further improve the path given by greedy solution
+- Compare the total distance of the path to shortest distance of previous paths, replace if shorter.
+
+The above process is repeated multiple times in the program. (From challenge 0~4, incresing the number of repetition does not take too much time; as for challenge 5, 10 times is decent while for challenge 6, generating one path takes time so tried with 5 trials.) To further improve the output, increasing the number of trials is suggested, although it takes a while to complete challenge 5 and 6.
+
--- Week 5/solver_modified_greedy.py
+++ Week 5/solver_modified_greedy.py
@@ -1,4 +1,5 @@
 import math
+import random
 from common import print_tour, read_input
 from collections import deque
 
@@ -8,23 +9,18 @@ def find_distance(city1, city2) -> float:
 def total_distance(path, input):
   total_dist = 0.0
   for index in range(len(path)):
-    total_dist += find_distance(input[index-1], input[index])
+    total_dist += find_distance(input[path[index-1]], input[path[index]])
   return total_dist
 
 def two_opt(path, input):
   best = path
-  improved = True
-  while improved:
-    improved = False
-    for i in range(1, len(path)-2):
-      for j in range(i+1, len(path)):
-        if j-1 == 1: continue
-        new_route = path[:]
-        new_route[i:j] = path[j-1:i-1:-1]
-        if total_distance(new_route, input) < total_distance(path, input):
-          best = new_route
-          improved = True
-    path = best
+  for i in range(1, len(path)-2):
+    for j in range(i+1, len(path)):
+      if j-1 == 1: continue
+      new_route = path[:]
+      new_route[i:j] = path[j-1:i-1:-1]
+      if total_distance(new_route, input) < total_distance(path, input):
+        best = new_route
   return best
 
 def greedy_algorithm(input):
@@ -35,29 +31,34 @@ def greedy_algorithm(input):
     for j in range(i, N):
       dist[i][j] = dist[j][i] = find_distance(input[i], input[j])
 
-  current_city = 0
-  unvisited_cities = set(range(1, N))
-  path = [current_city]
+  best_path = []
+  best_dist = 1000000
+  for i in range(0, 5):
+    current_city = random.randint(0, N-1)
+    print("Trial No.", i+1, "Starting from node:", current_city)
+    unvisited_cities = set(range(0, N))
+    unvisited_cities.remove(current_city)
+    path = [current_city]
 
-  while unvisited_cities:
-    next_city = min(unvisited_cities, key=lambda city: dist[current_city][city])
-    unvisited_cities.remove(next_city)
-    path.append(next_city)
-    current_city = next_city
-  return path
+    while unvisited_cities:
+      next_city = min(unvisited_cities, key=lambda city: dist[current_city][city])
+      unvisited_cities.remove(next_city)
+      path.append(next_city)
+      current_city = next_city
+    path = two_opt(path, input)
+    if total_distance(path, input) < best_dist:
+      best_path = path
+  return best_path
 
 def write_output (path: list[int]):
-  output = open("output_3.csv", "w")
+  output = open("output_6.csv", "w")
   output.write("index\n")
   for node in path:
     output.write(str(node) + "\n")
   output.close
 
 if __name__ == '__main__':
-  input = read_input("input_3.csv")
+  input = read_input("input_6.csv")
   path = greedy_algorithm(input)
-  path = two_opt(path, input)
   write_output(path)
   print_tour(path)
-
-
